8469521328cfaa624eba5730006fb2b8e3903c6b
OriOfek <aa6055@bs.amalnet.k12.il>
2020-10-15 11:03:49 +0300

    finish

--- app/src/main/java/com/example/lesson5/Activity2.java
+++ app/src/main/java/com/example/lesson5/Activity2.java
@@ -1,23 +1,20 @@
 package com.example.lesson5;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.text.Layout;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class Activity2 extends AppCompatActivity {
     LinearLayout screen;
     int[] array;
     int[] colors;
-    Intent si;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         screen = (LinearLayout) findViewById(R.id.screen);
         array = new int[]{R.id.blue, R.id.white, R.id.red};
-        colors = new int[]{Color.BLUE, Color.WHITE, Color.RED};
+        colors = new int[]{Color.BLUE, Color.WHITE, Color.RED, Color.GREEN};
 
     }
 
@@ -33,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main, menu);
+        menu.add(0,0,400,"green");
         return true;
     }
 
@@ -49,13 +47,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
-        screen.setBackgroundColor(colors[getIndex(array, item.getItemId())]);
+        int index = getIndex(array, item.getItemId());
+
+        if(index != -1)
+        {
+            screen.setBackgroundColor(colors[index]);
+        }
+        else
+        {
+            screen.setBackgroundColor(colors[colors.length-1]);
+        }
 
         return true;
     }
 
-    public void next(View view) {
-        si = new Intent(this, Activity2.class);
-        startActivity(si);
+    public void per(View view) {
+        finish();
     }
 }
