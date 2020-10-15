034d869b80d66528e524677270647d054b9a20e7
OriOfek <aa6055@bs.amalnet.k12.il>
2020-10-15 10:53:51 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lesson5/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.lesson5;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintSet;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+    LinearLayout screen;
+    int[] array;
+    int[] colors;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        screen = (LinearLayout) findViewById(R.id.screen);
+        array = new int[]{R.id.blue, R.id.white, R.id.red};
+        colors = new int[]{Color.BLUE, Color.WHITE, Color.RED};
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    public  int getIndex(int[] array,int indexToFind)
+    {
+        for (int i = 0;i < array.length;i++)
+        {
+            if(array[i] == indexToFind)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        screen.setBackgroundColor(colors[getIndex(array, item.getItemId())]);
+
+        return true;
+    }
+}
